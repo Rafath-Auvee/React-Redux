dd0aab57f3db2c41973b53f3f799792d2835a8ad
Rafath Bin Zafar Auvee <61088535+Rafath-Bin-Zafar-Auvee@users.noreply.github.com>
2022-06-20 04:50:08 +0600

    state management

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import {useSelector} from "react-redux"
+import { useSelector, useDispatch } from "react-redux";
 
 function App() {
+  const counter = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+  const increment = () => {
+    dispatch({ type: "INC" });
+  }
+  
+  const decrement = () => {
+    dispatch({ type: "FUAD" });
+  }
+
   return (
     <div>
       <h1>Counter App</h1>
-    </div>    
+      <h2>{counter}</h2>
+      <div>
+        <button onClick={increment}>Increment</button>
+        <button onClick={decrement}>Decrement</button>
+      </div>
+    </div>
   );
 }
 
--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,14 @@
-import { createStore } from "redux";
+import { createStore, useDispatch } from "redux";
 
-const reducerFn = (state = { counter: 0 }, action) => {};
+const reducerFn = (state = { counter: 120 }, action) => {
+  if (action.type === "INC") {
+    return { counter: state.counter + 1 };
+  }
+  if (action.type === "FUAD") {
+    return { counter: state.counter - 1 };
+  }
+  return state;
+};
 
 const store = createStore(reducerFn);
 
